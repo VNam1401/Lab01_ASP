5ed859b9f36d3fc8490fef0a6226e13cc3484ea6
ADMIN <ADMIN@PC316>
2025-05-16 08:04:24 +0700

    Add project files.

---
+++ Lab01_ASP/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_ASP.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private IWebHostEnvironment hosting;
+        public RegisterController(IWebHostEnvironment _hosting)
+        {
+            hosting = _hosting;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult XuLy(Models.PersonViewModel m, IFormFile FHinh)
+        {
+            if (FHinh != null)
+            {
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(FHinh.FileName);
+                string path = Path.Combine(hosting.WebRootPath, "images");
+                using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                {
+                    FHinh.CopyTo(filestream);
+                }
+                m.Picture = filename;
+            }
+            return View(m);
+        }
+    }
+}
---
+++ Lab01_ASP/Models/PersonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01_ASP.Models
+{
+    public class PersonViewModel
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Picture { get; set; }
+    }
+}
